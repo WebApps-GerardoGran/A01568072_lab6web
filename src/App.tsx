098fb25f6531fefcf0f2dc3fe1b4169704d7b629
Gerardo Granados Aldaz <gerardogr.al@outlook.com>
2021-09-28 23:01:36 -0600

    replace ProductDetail for ProductListContainer

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import React from "react";
 import './App.css';
-import ProductDetail from './containers/ProductDetail';
+// import ProductDetail from './containers/ProductDetail';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Header from "./containers/Header";
+import ProductListContainer from "./containers/ProductListContainer";
 
 function App() {
   return (
     <React.Fragment>
       <CssBaseline />
-      <Header/>
-      <ProductDetail/>
+      <Header />
+      <ProductListContainer />
     </React.Fragment>
   );
 }
